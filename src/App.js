27d1faa079a4ef122918412c8c91bed70e287dc9
unknown <karya.tiosaputra@gmail.com>
2019-03-12 21:10:10 +0700

    Complete Todo, and delete Todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo, removeTodo }) {
   return (
     <div
       style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
       className="todo"
     >
       {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>X</button>
+      </div>
     </div>
   );
 }
@@ -56,11 +60,31 @@ function App() {
     setTodos(newTodos);
   };
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => {
-          return <Todo key={index} index={index} todo={todo} />;
+          return (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+            />
+          );
         })}
         <TodoForm addTodo={addTodo} />
       </div>
