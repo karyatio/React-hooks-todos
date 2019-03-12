1842a0bc86d81ec3e38b8e1cb201176bdba6fe44
unknown <karya.tiosaputra@gmail.com>
2019-03-12 21:03:53 +0700

    Adding todo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,37 @@ import React, { useState } from "react";
 import "./App.css";
 
 function Todo({ todo, index }) {
-  return <div className="todo">{todo.text}</div>;
+  return (
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      className="todo"
+    >
+      {todo.text}
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        placeholder="Add Todo"
+      />
+    </form>
+  );
 }
 
 function App() {
@@ -21,12 +51,18 @@ function App() {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => {
           return <Todo key={index} index={index} todo={todo} />;
         })}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
